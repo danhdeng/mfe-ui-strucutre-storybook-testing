6681a672d03696a0bb661b1bfcb5e4f89301a5c2
danhdeng <hui_danny_deng@hotmail.com>
2022-06-26 09:05:32 -0400

    fixed a bug

--- packages/card/types/routeType.ts
+++ packages/card/types/routeType.ts
@@ -1,4 +1,3 @@
-import { React } from 'react';
 
 export type NavRoute={
     element: React.FunctionComponent;
--- packages/movie-content/types/routeType.ts
+++ packages/movie-content/types/routeType.ts
@@ -1,5 +1,3 @@
-import { React } from 'react';
-
 export type NavRoute={
     element: React.FunctionComponent;
     path: string;
